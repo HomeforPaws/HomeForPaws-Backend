fd8c97525b61cd534eb7b6130babaa0a49d00912
Juhee <viimv2000@gmail.com>
2023-07-03 22:58:44 +0900

    feat: getAnimalBySpecies 구현

--- src/main/java/com/hfp/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/hfp/domain/user/domain/repository/UserRepository.java
@@ -3,13 +3,13 @@ package com.hfp.domain.user.domain.repository;
 import java.util.Optional;
 
 
-import com.hfp.domain.user.domain.User;
+import com.hfp.domain.user.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long>{
+public interface UserRepository extends JpaRepository<Users,Long>{
     
-    Optional<User> findByEmail(String email);
+    Optional<Users> findByEmail(String email);
     Boolean existsByEmail(String email);
 }
