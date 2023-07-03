38a54d6632258eb6c80e5d0525bdf8685af1da00
Park Sejin <jinpark99@naver.com>
2023-07-04 02:20:16 +0900

    [FEAT] S3Config, S3Uploader 추가

--- src/main/java/com/hfp/domain/animal/application/AnimalService.java
+++ src/main/java/com/hfp/domain/animal/application/AnimalService.java
@@ -3,14 +3,21 @@ package com.hfp.domain.animal.application;
 import com.hfp.domain.animal.domain.Animal;
 import com.hfp.domain.animal.domain.repository.AnimalRepository;
 import com.hfp.domain.animal.dto.GetAnimalRes;
+import com.hfp.domain.animal.dto.PostAnimalReq;
+import com.hfp.domain.user.domain.User;
+import com.hfp.domain.user.domain.repository.UserRepository;
 import com.hfp.global.error.DefaultException;
+import com.hfp.global.infrastructure.S3Uploader;
 import com.hfp.global.payload.ApiResponse;
 import com.hfp.global.payload.ErrorCode;
+import com.hfp.global.payload.Message;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,6 +27,8 @@ import java.util.Optional;
 public class AnimalService {
 
     private final AnimalRepository animalRepository;
+    private final UserRepository userRepository;
+    private final S3Uploader s3Uploader;
 
     public ResponseEntity<?> getAnimalList() {
         List<Animal> findAnimals = animalRepository.findAll();
@@ -60,10 +69,45 @@ public class AnimalService {
                         .image_url(animal.getImage_url())
                         .place(animal.getPlace())
                         .age(animal.getAge())
-                        .description(animal.getDescription()))
+                        .description(animal.getDescription())
+                        .build())
                 .build();
 
         return ResponseEntity.ok(apiResponse);
     }
-  
+
+    @Transactional
+    public ResponseEntity<?> postAnimal(PostAnimalReq postAnimalReq, MultipartFile image) throws IOException {
+
+        User user = userRepository.findById(postAnimalReq.getRescue_id())
+                .orElseThrow(() -> new DefaultException(ErrorCode.INVALID_CHECK));
+
+        Animal animal = Animal.builder()
+                .rescueUser(user)
+                .name(postAnimalReq.getName())
+                .species(postAnimalReq.getSpecies())
+                .gender(postAnimalReq.getGender())
+                .place(postAnimalReq.getPlace())
+                .age(postAnimalReq.getAge())
+                .description(postAnimalReq.getDescription())
+                .bank(postAnimalReq.getBank())
+                .account(postAnimalReq.getAccount())
+                .build();
+
+        // img가 비어있는지 체크
+        // 업로드할 디렉토리 이름 설정 (record의 이미지는 record_img, 프로필의 이미지는 profile_img
+        if (image != null) {
+            String storedFileName = s3Uploader.upload(image, "animal_image");
+            animal.updateImageUrl(storedFileName);
+        }
+
+        animalRepository.save(animal);
+
+        ApiResponse apiResponse = ApiResponse.builder()
+                .check(true)
+                .information(Message.builder().message("유기동물이 등록되었습니다.").build())
+                .build();
+
+        return ResponseEntity.ok(apiResponse);
+    }
 }
--- src/main/java/com/hfp/domain/animal/presentation/AnimalController.java
+++ src/main/java/com/hfp/domain/animal/presentation/AnimalController.java
@@ -1,12 +1,15 @@
 package com.hfp.domain.animal.presentation;
 
 import com.hfp.domain.animal.application.AnimalService;
+import com.hfp.domain.animal.dto.PostAnimalReq;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +28,12 @@ public class AnimalController {
             @PathVariable("animalId") Long animalId) {
         return animalService.getAnimal(animalId);
     }
-  
+
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<?> postAnimal(
+            @Valid @RequestPart("postAnimalReq")PostAnimalReq postAnimalReq,
+            @RequestPart(value = "image", required = false) MultipartFile image
+            )throws IOException {
+        return animalService.postAnimal(postAnimalReq, image);
+    }
 }
