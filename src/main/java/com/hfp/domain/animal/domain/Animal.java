e498b62655ab86ee035f419788669137a111b688
Sejin Park <95167215+sejineer@users.noreply.github.com>
2023-07-03 20:41:45 +0900

    [FEAT]: Animal 엔티티 내용 추가 (#3)

--- src/main/java/com/hfp/domain/animal/domain/Animal.java
+++ src/main/java/com/hfp/domain/animal/domain/Animal.java
@@ -1,4 +1,37 @@
 package com.hfp.domain.animal.domain;
 
-public class Animal {
+import com.hfp.domain.common.BaseEntity;
+import com.hfp.domain.user.domain.Users;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Animal extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String species;
+
+    private String gender;
+
+    private String image_url;
+
+    private String place;
+
+    private Integer age;
+
+    private String description;
+
+    private String bank;
+
+    private String account;
+
 }
