ae17ae8ba48061024adda418c7ab27cbc5128fbc
Park Sejin <jinpark99@naver.com>
2023-07-03 22:20:39 +0900

    [FIX]: GetAnimalRes DTO 수정

--- src/main/java/com/hfp/domain/animal/dto/GetAnimalRes.java
+++ src/main/java/com/hfp/domain/animal/dto/GetAnimalRes.java
@@ -2,7 +2,6 @@ package com.hfp.domain.animal.dto;
 
 import com.hfp.domain.animal.domain.Gender;
 import com.hfp.domain.animal.domain.Species;
-import com.hfp.domain.user.domain.Users;
 import lombok.Builder;
 import lombok.Data;
 
