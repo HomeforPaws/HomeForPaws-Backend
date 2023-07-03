905d992df9415bd9af1e15b6449aa0e0d0b7dc08
letskuku <yujung9288@naver.com>
2023-07-03 21:53:43 +0900

    Feat: getProof api 구현

--- src/main/java/com/hfp/domain/proof/domain/repository/ProofRepository.java
+++ src/main/java/com/hfp/domain/proof/domain/repository/ProofRepository.java
@@ -1,4 +1,11 @@
 package com.hfp.domain.proof.domain.repository;
 
-public interface ProofRepository {
+import com.hfp.domain.proof.domain.Proof;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProofRepository extends JpaRepository<Proof, Long> {
+
+    Optional<Proof> findProofById(Long animal_id);
 }
