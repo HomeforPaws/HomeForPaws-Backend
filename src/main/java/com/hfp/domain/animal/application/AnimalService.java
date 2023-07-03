fdd61790e2a03e239e1e7b6ae2772102aafd00a9
Park Sejin <jinpark99@naver.com>
2023-07-03 21:22:35 +0900

    [FEAT]: getAnimal API 구현

--- src/main/java/com/hfp/domain/animal/application/AnimalService.java
+++ src/main/java/com/hfp/domain/animal/application/AnimalService.java
@@ -2,15 +2,20 @@ package com.hfp.domain.animal.application;
 
 import com.hfp.domain.animal.domain.Animal;
 import com.hfp.domain.animal.domain.repository.AnimalRepository;
+import com.hfp.global.error.DefaultException;
 import com.hfp.global.payload.ApiResponse;
+import com.hfp.global.payload.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class AnimalService {
 
     private final AnimalRepository animalRepository;
@@ -25,4 +30,15 @@ public class AnimalService {
         return ResponseEntity.ok(apiResponse);
     }
 
+    public ResponseEntity<?> getAnimal(Long animalId) {
+        Animal animal = animalRepository.findById(animalId)
+                .orElseThrow(() -> new DefaultException(ErrorCode.INVALID_CHECK));
+
+        ApiResponse apiResponse = ApiResponse.builder()
+                .check(true)
+                .information(animal)
+                .build();
+
+        return ResponseEntity.ok(apiResponse);
+    }
 }
