90ebf6550e6aa8674bda6d762c6906b94dbf3b1e
letskuku <yujung9288@naver.com>
2023-07-03 20:40:15 +0900

    Feat: 후원 증빙 엔티티

--- src/main/java/com/hfp/domain/proof/domain/Proof.java
+++ src/main/java/com/hfp/domain/proof/domain/Proof.java
@@ -1,4 +1,33 @@
 package com.hfp.domain.proof.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Proof {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long proof_id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "animal_id")
+    private Long animal_id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "rescue_id")
+    private Long rescue_id;
+
+    @Column(nullable = false)
+    private String proof_url;
+
+    @Column(nullable = false)
+    private String recent_url;
+
+    @Column(nullable = false)
+    private String description;
 }
