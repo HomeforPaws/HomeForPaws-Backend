67e2a1bc54d3beb111fde1fffafb6d0e9ab1d879
Juhee <viimv2000@gmail.com>
2023-07-04 01:05:06 +0900

    error: getUsers 오류

--- src/main/java/com/hfp/domain/user/application/UserService.java
+++ src/main/java/com/hfp/domain/user/application/UserService.java
@@ -1,8 +1,15 @@
 package com.hfp.domain.user.application;
 
+import com.hfp.domain.animal.domain.Animal;
+import com.hfp.domain.animal.dto.GetAnimalRes;
 import com.hfp.domain.user.domain.Users;
+import com.hfp.domain.user.domain.dto.GetUsersRes;
 import com.hfp.domain.user.domain.repository.UserRepository;
+import com.hfp.global.error.DefaultException;
+import com.hfp.global.payload.ApiResponse;
+import com.hfp.global.payload.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,30 +23,26 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    /**
-     * 회원 가입
-     */
-    @Transactional
-    public Long join(Users users) {
-
-        validateDuplicateUsers(users); //중복 회원 검증
-        userRepository.save(users);
-        return users.getId();
-    }
-
-    private void validateDuplicateUsers(Users users) {
-        Optional<Users> findUsers = userRepository.findByEmail(users.getEmail());
-        if (!findUsers.isEmpty()) {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
-        }
-    }
 
     //회원 전체 조회
     public List<Users> findUsers() {
         return userRepository.findAll();
     }
 
-    public Optional<Users> findOne(Long userId) {
-        return userRepository.findById(userId);
+
+    public ResponseEntity<?> getUser(Long userId) {
+        Users users = userRepository.findById(userId)
+                .orElseThrow(() -> new DefaultException(ErrorCode.INVALID_CHECK));
+
+        ApiResponse apiResponse = ApiResponse.builder()
+                .check(true)
+                .information(GetUsersRes.builder()
+                        .name(users.getName())
+                        .nickname(users.getNickname())
+                        .phone(users.getPhone())
+                        .email(users.getEmail()))
+                .build();
+
+        return ResponseEntity.ok(apiResponse);
     }
 }
--- src/main/java/com/hfp/domain/user/presentation/UserController.java
+++ src/main/java/com/hfp/domain/user/presentation/UserController.java
@@ -1,37 +1,24 @@
 package com.hfp.domain.user.presentation;
 
 import com.hfp.domain.user.application.UserService;
-import com.hfp.domain.user.domain.Users;
-import com.hfp.domain.user.domain.dto.UserDto;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/user/{userId}")
-    public Result getUser(@PathVariable Long userId) {
-
-        Optional<Users> findUser = userService.findOne(userId);
-        //엔티티 -> DTO 변환
-        Users result = (Users) findUser.stream()
-                .map(m -> new UserDto(m.getName(), m.getNickname(), m.getPhone(), m.getEmail()));
-
-        return new Result(findUser);
-    }
-
-    @Data
-    @AllArgsConstructor
-    static class Result<T> {
-        private T data;
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getUser(
+            @PathVariable("userId") Long userId) {
+        return userService.getUser(userId);
     }
 
 }
