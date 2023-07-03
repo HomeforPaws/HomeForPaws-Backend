1dd13dfb5859f99c7cb0fe34f7ba8e037a70ab4a
Park Sejin <jinpark99@naver.com>
2023-07-03 21:03:33 +0900

    [FEAT]: getAnimalList API 구현

--- src/main/java/com/hfp/domain/animal/domain/Animal.java
+++ src/main/java/com/hfp/domain/animal/domain/Animal.java
@@ -4,6 +4,7 @@ import com.hfp.domain.common.BaseEntity;
 import com.hfp.domain.user.domain.Users;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -39,5 +40,19 @@ public class Animal extends BaseEntity {
     private String bank;
 
     private String account;
+
+    @Builder
+    public Animal(Users rescueUser, String name, Species species, Gender gender, String image_url, String place, Integer age, String description, String bank, String account) {
+        this.rescueUser = rescueUser;
+        this.name = name;
+        this.species = species;
+        this.gender = gender;
+        this.image_url = image_url;
+        this.place = place;
+        this.age = age;
+        this.description = description;
+        this.bank = bank;
+        this.account = account;
+    }
 
 }
--- src/main/java/com/hfp/domain/animal/domain/repository/AnimalRepository.java
+++ src/main/java/com/hfp/domain/animal/domain/repository/AnimalRepository.java
@@ -1,4 +1,7 @@
 package com.hfp.domain.animal.domain.repository;
 
-public interface AnimalRepository {
+import com.hfp.domain.animal.domain.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
 }
